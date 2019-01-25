e745186f7a80efc0f22f5a0fc1d934560ffc6dc7
liciascl <licia.sales@hotmail.com>
2019-01-25 09:56:01 -0200

    up

---
+++ media/imagens/despertador.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#Script despertador
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+sleep 1.5 ;
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+sleep 1.5 ;
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+sleep 1.5 ;
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+sleep 600 ; #Função soneca, 600 equivale a 10 minutos.
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+sleep 1.5 ;
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+sleep 1.5 ;
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+sleep 1.5 ;
+
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+beep -f 2000 -l 200
+beep -f 2000 -l 100
+
+#Fim do script
